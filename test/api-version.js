66f44823e0e448de77059cf7c74086f4ada03ffb
gaara <sabakugaara@users.noreply.github.com>
2017-02-07 10:29:28 +0800

    feat use standard js style (#15)
    fix issues/14

--- test/api-version.js
+++ test/api-version.js
@@ -1,15 +1,14 @@
-'use strict';
-var UPYUN = require('..');
-var should = require('should');
+'use strict'
+var UPYUN = require('..')
 
-describe('API Version: ', function() {
-    it('should return instance with legacy API', function() {
-        var upyun = new UPYUN('travis', 'travisci', 'testtest', 'v3', 'legacy');
-        upyun._apiVersion.should.be.exactly('legacy');
-    });
+describe('API Version: ', function () {
+  it('should return instance with legacy API', function () {
+    var upyun = new UPYUN('travis', 'travisci', 'testtest', 'v3', 'legacy')
+    upyun._apiVersion.should.be.exactly('legacy')
+  })
 
-    it('should return instance with latest API', function() {
-        var upyun = new UPYUN('travis', 'travisci', 'testtest', 'v3', 'latest');
-        upyun._apiVersion.should.be.exactly('latest');
-    });
-});
+  it('should return instance with latest API', function () {
+    var upyun = new UPYUN('travis', 'travisci', 'testtest', 'v3', 'latest')
+    upyun._apiVersion.should.be.exactly('latest')
+  })
+})
