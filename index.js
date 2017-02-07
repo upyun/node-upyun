36081cc6cd13b6170094827847565d422f2a24b3
Gaara <sabakugaara@users.noreply.github.com>
2017-02-07 10:24:32 +0800

    feat use standard js style
    fix issues/14

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-'use strict';
-var upyunV1 = require('upyun-legacy');
-var upyunV2 = require('./lib/api.js');
+'use strict'
+var UpyunV1 = require('upyun-legacy')
+var UpyunV2 = require('./lib/api.js')
 
-module.exports = exports.UPYUN = function(bucket, operator, password, endpoint, apiVersion) {
+module.exports = exports.UPYUN = function (bucket, operator, password, endpoint, apiVersion) {
     // wait for the latest api online
-    apiVersion = apiVersion || 'legacy';
+  apiVersion = apiVersion || 'legacy'
 
-    var client = null;
+  var client = null
 
-    switch(apiVersion.toLowerCase()) {
-        case 'legacy':
-            client =  new upyunV1(bucket, operator, password, endpoint);
-            break;
-        case 'latest':
-            client =  new upyunV2(bucket, operator, password, endpoint);
-            break;
-        default:
-            client =  new upyunV1(bucket, operator, password, endpoint);
-            break;
-    }
+  switch (apiVersion.toLowerCase()) {
+    case 'legacy':
+      client = new UpyunV1(bucket, operator, password, endpoint)
+      break
+    case 'latest':
+      client = new UpyunV2(bucket, operator, password, endpoint)
+      break
+    default:
+      client = new UpyunV1(bucket, operator, password, endpoint)
+      break
+  }
 
-    client._apiVersion = apiVersion;
+  client._apiVersion = apiVersion
 
-    return client;
-};
+  return client
+}
